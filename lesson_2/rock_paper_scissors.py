7bfe8bd01da1666fc5e0162470769532166e94af
Joel Turske <joelturske@gmail.com>
2024-12-27 17:11:33 -0500

    added option to continue playing and abstracted winner determination into a function.

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -5,25 +5,37 @@ VALID_CHOICES = ['rock', 'paper', 'scissors']
 def prompt(message):
     print(f"==> {message}")
 
+def declare_winner(choice, computer_choice):
+    if choice == computer_choice:
+        prompt("It was a tie!")
+    elif ((choice == 'rock' and computer_choice == 'scissors') or
+        (choice == 'paper' and computer_choice == 'rock') or
+        (choice == 'scissors' and computer_choice == 'paper')):
+        prompt(f"You win! {choice} beat {computer_choice}.")
+    else:
+        prompt(f"The computer wins! {computer_choice} beats {choice}.")
+
 prompt("Welcome to Rock, Paper, Scissors!\n")
 
-# Add looping logic so play can continue! 
-prompt(f"Please choose one: {', '.join(VALID_CHOICES)}")
-choice = input()
+while True:
+    prompt(f"Please choose one: {', '.join(VALID_CHOICES)}")
+    user_choice = input()
 
-while choice not in VALID_CHOICES:
-    prompt("That's not a valid choice.")
-    choice = input()
+    while user_choicechoice not in VALID_CHOICES:
+        prompt("That's not a valid choice.")
+        user_choicechoice = input()
 
-computer_choice = random.choice(VALID_CHOICES)
+    cpu_choice = random.choice(VALID_CHOICES)
 
-prompt(f"You chose: {choice}, the computer chose: {computer_choice}.")
-# Abstract this to a determine winner function
-if choice == computer_choice:
-    prompt("It was a tie!")
-elif ((choice == 'rock' and computer_choice == 'scissors') or
-    (choice == 'paper' and computer_choice == 'rock') or
-    (choice == 'scissors' and computer_choice == 'paper')):
-    prompt(f"User wins! {choice} beat {computer_choice}.")
-else:
-    prompt(f"The computer wins! {computer_choice} beats {choice}.")
+    prompt(f"You chose: {user_choice}, the computer chose: {cpu_choice}.")
+    # Abstract this to a determine winner function
+    declare_winner(user_choice, cpu_choice)
+    
+    prompt("Would you like to play again? Enter (y/n)")
+    answer = input()
+    if answer.casefold() not in ['y', 'yes', 'n', 'no']:
+        prompt("Invalid response. Please enter y or n.")
+        answer = input()
+
+    if answer.casefold() in ['n', 'no']:
+        break
