9838e92abd4504ecfcd6ff4fe0cc61ecdb4137e3
Joel Turske <joelturske@gmail.com>
2024-12-31 10:47:54 -0500

    Question 3 in easy 1 of Lesson 3.

---
+++ lesson_3/famous_words.py
@@ -0,0 +1,9 @@
+famous_words = "seven years ago..."
+
+# Solution 1
+print("Four score and" + ' ' + famous_words)
+
+#Solution 2
+famous_words = "seven years ago..."
+
+print(' '.join(['Four score and', famous_words]))
