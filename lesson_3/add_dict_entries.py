5ade9cbe95959eb3a92e8b8c8b8b874ce916b798
Joel Turske <joelturske@gmail.com>
2025-01-01 11:05:46 -0500

    Q9 in easy 2, lesson 3. Upated a dict with entries from another dict object.

---
+++ lesson_3/add_dict_entries.py
@@ -0,0 +1,12 @@
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 5843, 'Eddie': 10}
+additional_ages = {'Marilyn': 22, 'Spot': 237}
+
+ages.update(additional_ages)
+print(ages)
+
+
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 5843, 'Eddie': 10}
+additional_ages = {'Marilyn': 22, 'Spot': 237}
+
+ages |= additional_ages
+print(ages)
