6728f791741b75fa5c6e14315962fa264189c36c
Joel Turske <joelturske@gmail.com>
2025-01-01 09:46:17 -0500

    First question in easy 2, lesson 3. Reverse a list without mutating it.

---
+++ lesson_3/rev_list_no_mutate.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]     # [5, 4, 3, 2, 1]
+
+nums = list(reversed(numbers))
+print(nums)
+print(numbers)
+
+numbers = [1, 2, 3, 4, 5]     # [5, 4, 3, 2, 1]
+
+nums = numbers[::-1]
+print(nums)
+print(numbers)
